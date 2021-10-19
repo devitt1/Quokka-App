e8477f4896ac350e44a23f8766e358ce20e21f9e
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2021-10-19 11:17:09 +1100

    added HotspotConnectionPage

--- src/TheQDeviceConnect.UI/Pages/HotspotConnectionPage.xaml.cs
+++ src/TheQDeviceConnect.UI/Pages/HotspotConnectionPage.xaml.cs
@@ -20,7 +20,8 @@ namespace TheQDeviceConnect.UI.Pages
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
             _deviceConnectionService = DependencyService.Get<IDeviceConnectionService>();
-            _deviceConnectionService.Initialize(); 
+            _deviceConnectionService.Initialize();
+            _deviceConnectionService.OnWifiNetworkChanged += handleWifiNetworkChanged;
         }
 
         protected override void OnAppearing()
@@ -31,16 +32,25 @@ namespace TheQDeviceConnect.UI.Pages
         protected override void OnViewModelSet()
         {
             base.OnViewModelSet();
-            this.ViewModel.PropertyChanged -= ViewModel_PropertyChanged;
-            this.ViewModel.PropertyChanged += ViewModel_PropertyChanged;
         }
 
-        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+       
+        void handleWifiNetworkChanged(object sender, EventArgs eventArgs)
         {
-            if (e.PropertyName == "NetworkChanged")
+            DebugHelper.Info(this, "Connection changed!");
+            if (_deviceConnectionService.IsConnectedToHotspot())
             {
-                DebugHelper.Info(this, "called");
-                _deviceConnectionService.GetCurrentNetwork();
+                DebugHelper.Info(this, "Connected to The Q Hotspot!");
+                ViewModel.WifiConnectionState = HotspotConnectionViewModel.WifiNetworkConnectionState.HOTSPOT_CONNECTED;
+                ViewModel.ShouldDisplayConfirmConnectedPage = true;
+            }
+            else if (ViewModel.WifiConnectionState == HotspotConnectionViewModel.WifiNetworkConnectionState.CONNECTING) 
+            {
+                ViewModel.ShouldDisplayConnectingPage = true;
+            }
+            else if (ViewModel.WifiConnectionState == HotspotConnectionViewModel.WifiNetworkConnectionState.DEFAULT)
+            {
+                ViewModel.ShouldDisplayConnectingPage = false;
             }
         }
     }
