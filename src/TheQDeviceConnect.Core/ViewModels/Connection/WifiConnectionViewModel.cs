df7333ac2a56f0a7398b3cdd44382377fa68149e
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2021-10-19 16:57:19 +1100

    added WifiConnectionPage and style, elements for WifiNetworkViewModels RepeaterView

---
+++ src/TheQDeviceConnect.Core/ViewModels/Connection/WifiConnectionViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using static TheQDeviceConnect.Core.ViewModels.Connection.Hotspot.HotspotConnectionViewModel;
+
+namespace TheQDeviceConnect.Core.ViewModels.Connection
+{
+    public class WifiConnectionViewModel : BaseNavigationViewModel
+    {
+        public WifiConnectionViewModel(ILoggerFactory logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
+        {
+            addMockedData();
+        }
+
+        private void addMockedData()
+        {
+            WifiNetworkVMs.Add(new WifiNetworkViewModel()
+            {
+                ssid = "TheQHotSpot"
+            });
+            WifiNetworkVMs.Add(new WifiNetworkViewModel()
+            {
+                ssid = "Kogan"
+            });
+        }
+
+        private WifiNetworkConnectionState _wifiConnectionState;
+        public WifiNetworkConnectionState WifiConnectionState
+        {
+            get
+            {
+                return _wifiConnectionState;
+            }
+            set
+            {
+                _wifiConnectionState = value;
+                RaisePropertyChanged(() => WifiConnectionState);
+            }
+        }
+
+        private MvxObservableCollection<IWifiNetworkViewModel> _wifiNetworkVMs = new MvxObservableCollection<IWifiNetworkViewModel>();
+        public MvxObservableCollection<IWifiNetworkViewModel> WifiNetworkVMs
+        {
+            get => _wifiNetworkVMs;
+            set
+            {
+                _wifiNetworkVMs = value;
+                RaisePropertyChanged(() => WifiNetworkVMs);
+            }
+        }
+
+
+
+    }
+}
---
+++ src/TheQDeviceConnect.UI/Pages/WifiConnectionPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using MvvmCross.Forms.Views;
+using TheQDeviceConnect.Core.ViewModels.Connection;
+using Xamarin.Forms;
+
+namespace TheQDeviceConnect.UI.Pages
+{
+    public partial class WifiConnectionPage : MvxContentPage<WifiConnectionViewModel>
+    {
+        public WifiConnectionPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+    }
+}
