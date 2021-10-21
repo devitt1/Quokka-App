499440dbec523d33ab05f13de9e6c31fb7a2b62e
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2021-10-21 18:21:12 +1100

    implemented property change on BaseViewModel

--- src/TheQDeviceConnect.Core/ViewModels/BaseViewModel.cs
+++ src/TheQDeviceConnect.Core/ViewModels/BaseViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using MvvmCross.ViewModels;
 
@@ -7,5 +8,17 @@ namespace TheQDeviceConnect.Core.ViewModels
 {
     public abstract class BaseViewModel : MvxViewModel
     {
+        public BaseViewModel()
+        {
+
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        // MVVM Commands
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
