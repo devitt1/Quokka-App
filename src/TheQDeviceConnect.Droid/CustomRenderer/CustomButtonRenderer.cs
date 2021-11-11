10dc64565e92c606dd47a4c71d4e30c2103f87d5
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2021-11-11 13:37:24 +1100

    Added custom renderers for button and entry on Android

---
+++ src/TheQDeviceConnect.Droid/CustomRenderer/CustomButtonRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.Content;
+using TheQDeviceConnect.Droid.CustomRenderer;
+using TheQDeviceConnect.UI.CustomUI;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
+namespace TheQDeviceConnect.Droid.CustomRenderer
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        public bool IsUnderline { private get; set; }
+        public CustomButtonRenderer(Context context) : base(context)
+        {
+            
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            CustomButton newElement = (CustomButton)e.NewElement;
+            if (Control != null)
+            {
+                if (newElement.IsUnderline)
+                {
+                    Control.PaintFlags = Android.Graphics.PaintFlags.UnderlineText;
+                }
+                if (!newElement.IsEnabled)
+                {
+                    Control.SetTextColor(Android.Graphics.Color.White);
+                }
+            }
+        }
+
+    }
+}
---
+++ src/TheQDeviceConnect.Droid/CustomRenderer/CustomEntryRenderer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Text;
+using Android.Widget;
+using TheQDeviceConnect.Droid.CustomRenderer;
+using TheQDeviceConnect.UI.CustomUI;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using AndroidGraphicsColor = Android.Graphics.Color;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace TheQDeviceConnect.Droid.CustomRenderer
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        [Obsolete]
+        #pragma warning disable CS0809 // Obsolete member overrides non-obsolete member
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        #pragma warning restore CS0809 // Obsolete member overrides non-obsolete member
+        {
+            base.OnElementChanged(e);
+           
+            if (Control != null) {
+                
+                Control.Elevation = 10;
+                Control.TextSize = 30;
+                Control.SetPadding(100, 0, 100, 0);
+                Control.SetOutlineSpotShadowColor(AndroidGraphicsColor.Black);
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+                    Control.BackgroundTintList = ColorStateList.ValueOf(AndroidGraphicsColor.White);
+                else
+                    Control.Background.SetColorFilter(AndroidGraphicsColor.White, PorterDuff.Mode.SrcAtop);
+
+            }
+        }
+    }
+}
