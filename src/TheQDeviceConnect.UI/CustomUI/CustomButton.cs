a5861e0ac204e170a7fad4fb407c4d4c1da55d62
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2021-11-11 13:51:43 +1100

    Added custom UI fields for XAML usage

---
+++ src/TheQDeviceConnect.UI/CustomUI/CustomButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TheQDeviceConnect.UI.CustomUI
+{
+    public class CustomButton : Button
+    {
+        public bool IsUnderline { get; set; }
+        public CustomButton()
+        {
+        }
+    }
+}
---
+++ src/TheQDeviceConnect.UI/CustomUI/CustomEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TheQDeviceConnect.UI.CustomUI
+{
+    public class CustomEntry : Entry
+    {
+        public CustomEntry()
+        {
+        }
+    }
+}
