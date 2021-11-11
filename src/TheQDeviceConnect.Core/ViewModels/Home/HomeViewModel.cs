a325f3341aa8c155829933abf4edd0ba82c51faa
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2021-11-11 13:33:21 +1100

    Added use case in HomeViewModel with deviceConnectionService, added request permissions for scanning nearby wifi networks, android nsd implementation, service resolved handler and saving to device local storage, stop mDNS discovery process after user exit view model

--- src/TheQDeviceConnect.Core/ViewModels/Home/HomeViewModel.cs
+++ src/TheQDeviceConnect.Core/ViewModels/Home/HomeViewModel.cs
@@ -1,8 +1,16 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using MvvmCross;
 using MvvmCross.Commands;
 using MvvmCross.Navigation;
+using TheQDeviceConnect.Core.Helpers;
+using TheQDeviceConnect.Core.Services.Interfaces;
+using TheQDeviceConnect.Core.Utils;
+using TheQDeviceConnect.Core.ViewModels.Connection;
 using TheQDeviceConnect.Core.ViewModels.Connection.Hotspot;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 
 namespace TheQDeviceConnect.Core.ViewModels.Home
@@ -13,18 +21,79 @@ namespace TheQDeviceConnect.Core.ViewModels.Home
         public HomeViewModel(ILoggerFactory logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
         {
             GoToHotspotConnectionViewModelCommand = new MvxAsyncCommand(GoToHotspotConnectionVMAsync, allowConcurrentExecutions: true);
+            _deviceConnectionService = DependencyService.Get<IDeviceConnectionService>();
+            _coreDeviceConnectionService = Mvx.IoCProvider.Resolve<IDeviceConnectionService>();
+            _deviceConnectionService.OnAndroidNsdResolved += handleAndroidNsdResolved;
+
         }
 
         public MvxAsyncCommand GoToHotspotConnectionViewModelCommand { get; private set; }
+        IDeviceConnectionService _coreDeviceConnectionService; //Service from core
+        IDeviceConnectionService _deviceConnectionService; // Native
 
         private async Task GoToHotspotConnectionVMAsync()
         {
-            await NavigationService.Navigate<HotspotConnectionViewModel>();
+            await Permissions.RequestAsync<Permissions.StorageWrite>();
+            await Permissions.RequestAsync<Permissions.StorageRead>();
+            var status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            
+            if (SecureStorage.GetAsync("DeviceResolvedLocalAddress").Result == null)
+            {
+                await NavigationService.Navigate<HotspotConnectionViewModel>();
+            }
+            else
+            {
+                await NavigationService.Navigate<WifiConnectionViewModel>();
+            }
         }
+
+        public override Task Initialize()
+        {
+            try
+            {
+                _deviceConnectionService.InitializeAndroidNsd();
+            }
+            catch (Exception e)
+            {
+                DebugHelper.Error(this, e);
+
+            }
+            return base.Initialize();
+        }
+
+        private void handleAndroidNsdResolved(object sender, EventArgs eventArgs)
+        {
+            SimpleServiceResolvedEventArgs e = eventArgs as SimpleServiceResolvedEventArgs;
+            DebugHelper.Info(this, "handleAndroidNsdResolved, Host = " + e.Host);
+            SecureStorage.SetAsync("DeviceResolvedLocalAddress", e.Host);
+        }
+
         public override void ViewAppeared()
         {
             base.ViewAppeared();
+            _deviceConnectionService.DiscoverNeabymDNSServices();
+            //try
+            //{
+            //    if (SecureStorage.GetAsync("DeviceResolvedLocalAddress").Result == null)
+            //    {
+            //        _deviceConnectionService.DiscoverNeabymDNSServices();
+            //    }
+            //    else
+            //    {
+            //        NavigationService.Navigate<WifiConnectionViewModel>();
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    DebugHelper.Error(this, e);
 
+            //}
+        }
+
+        public override void ViewDisappeared()
+        {
+            base.ViewDisappeared();
+            _deviceConnectionService.StopDiscoverNearbymDNSServices();
         }
     }
 }
