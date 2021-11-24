b75ad2b33752788a6f9edada7671213d7a64b9ae
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2021-11-25 10:12:47 +1100

    updated AppDelegate for initialise Lottie Animation

--- src/TheQDeviceConnect.iOS/AppDelegate.cs
+++ src/TheQDeviceConnect.iOS/AppDelegate.cs
@@ -1,10 +1,19 @@
 ﻿using Foundation;
+using Lottie.Forms.Platforms.Ios;
 using MvvmCross.Forms.Platforms.Ios.Core;
+using UIKit;
 
 namespace TheQDeviceConnect.iOS
 {
     [Register(nameof(AppDelegate))]
     public partial class AppDelegate : MvxFormsApplicationDelegate<Setup, Core.App, UI.App>
     {
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
+
+            return base.FinishedLaunching(uiApplication, launchOptions);
+        }
     }
 }
